0c526491692ae80a73e0e0f7e33b7ad41d2a5939
Matthijs07g <92449265+Matthijs07g@users.noreply.github.com>
2025-02-27 23:34:06 +0100

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const Asurascans_1 = require("./sources/Asurascans/Asurascans");
-module.exports = [
-    {
-        source: Asurascans_1.AsuraScans,
-        info: Asurascans_1.AsuraScansInfo
-    }
-];
